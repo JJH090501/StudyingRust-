07ef86994ac0b412a304493968405c7ad315c828
JJH090501 <cruisesa542@gmail.com>
2026-09-02 17:29:31 +0900

    feat: loop studied

--- Loop/case01.rs
+++ Loop/case01.rs
@@ -1,3 +1,26 @@
 fn main() {
-    
+
+    let mut cnt = 2; // mut is constant variable. You can change it.
+
+    let result = loop {
+        print!("Hello, Lua!")
+        println!(" I am Rust!")
+
+        cnt += 1;
+
+        if cnt == 3 {
+            continue;
+        }
+
+        println!("cnt={}", cnt);
+
+        if cnt == 5 {
+            break cnt; // break can return value.
+        }
+
+    };
+
+    println!("result={}", result);
 }
+
+// this is real infinite loop. Xpp u can stop it with break;
