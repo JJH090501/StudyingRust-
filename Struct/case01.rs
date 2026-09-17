5ebbc96b6eea3ebb9c83d1750935a2a46fa778ca
JJH090501 <cruisesa542@gmail.com>
2026-09-17 12:37:39 +0900

    Add Struct/case01.rs

---
+++ Struct/case01.rs
@@ -0,0 +1,18 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64;
+}
+
+fn main() {
+    let user1 = User {
+        active: true,
+        username: "yongin",
+        email: "yongin1212",
+        sign_in_count: 1,
+    };
+
+    
+}
+
