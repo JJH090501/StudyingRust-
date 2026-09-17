de3adccf0a7aa6750665ab28f32a7f0b8713c1e3
JJH090501 <cruisesa542@gmail.com>
2026-09-17 16:54:02 +0900

    Add Enums/case01.rs

---
+++ Enums/case01.rs
@@ -0,0 +1,15 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(&four);
+    route(&six);
+}
+
+fn route(ip_kind: &IpAddrKind) {}
+
