3559e00c702201efd3c8dc7e5ce8b57888f51a9a
JJH090501 <cruisesa542@gmail.com>
2026-09-17 16:54:02 +0900

    Add If_let/case01.rs

---
+++ If_let/case01.rs
@@ -0,0 +1,16 @@
+// this is match case
+
+// fn main() {
+//     let config_max = Some(3u8);
+//     match config_max {
+//         Some(max) => println!("The maximum is configured to be {}", max),
+//         _ => (),
+//     }
+// }
+
+fn main() {
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+}
